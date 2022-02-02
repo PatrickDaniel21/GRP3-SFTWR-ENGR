030d3fb9f93f1cd47a24a97e2d8cadd1dd889ef4
StoneWarrior108 <stonewarrior108@gmail.com>
2022-02-02 21:43:53 +0800

    Update 1.6

--- application/views/contact.php
+++ application/views/contact.php
@@ -11,7 +11,144 @@
     <title>C.L.I.K.I.T</title>
 
 	<!-- CSS STYLE FOR SIGN UP -->
+	<style type="text/css">
+		@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,500;0,600;0,700;1,400&display=swap');
+		*{
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+			outline: none;
+			text-decoration: none;
+			list-style: none;
+			font-family: 'Poppins', sans-serif;
+		}
 
+		body{
+			background-image: url( <?= base_url() ?>assets/img/bg10.jpg );
+        	background-repeat: no-repeat;
+			background-size: cover;
+		}
+
+		/* WRAPPER FOR LOGO AND FORM */
+		.wrapper{
+			display: flex;
+			width: 70rem;
+			height: 570px;
+			position: absolute;
+			top: 50%;
+			left:50%;
+			transform: translate(-50%,-50%);
+		}
+
+		/* SIZE OF BOX IN FORM */
+		.wrapper .right{
+			width: 65%;
+			height: 100%;
+			padding: 30px;
+			padding-left: 60px;
+			padding-right: 60px;
+			background: #632626;
+			background-repeat: no-repeat;
+			background-size: cover;
+			border-radius: 0 20px 20px 0;
+		}
+
+		/* SIZE OF BOX IN LOGO */
+		.wrapper .left{
+			width: 35%;
+			height: 100%;
+			padding: 30px;
+			background-image: url( <?= base_url() ?>assets/img/bg5.jpg );
+			background-repeat: no-repeat;
+			background-size: cover;
+			border-radius: 20px 0 0 20px ;
+		}
+
+		.wrapper .left img{
+			width: 100%;
+			transform: translate(1%,250%);
+		}
+
+		/* FORM GREETINGS */
+		.wrapper .right h3{
+			font-size: 2.5rem;
+			color: #fff3f2;
+			font-weight: 600;
+			line-height: 72px;
+			letter-spacing: 1px;
+			word-spacing: 2px;
+			text-align: center;
+			margin-bottom: 15px;
+			margin-top: 10px;
+		}
+
+		.wrapper .right .form{
+			margin-top: 0;
+		}
+
+		/* TITLE OF FORM QUESTIONS */
+		.wrapper .right .form label{
+			color: #fff3f2;
+			letter-spacing: 1px;
+			margin-top: 3px;
+			font-weight: 500;
+			font-size: 14px;
+		}
+
+		/* LOGIN QUESTION TEXT */
+		.wrapper .right h4{
+			color: #fff3f2;
+			font-size: small;
+			letter-spacing: 1px;
+			word-spacing: 1px;
+			text-align: center;
+		}
+
+		/* INPUT BOX */
+		.wrapper .right .input{
+			width: 100%;
+			padding: 3px;
+			border: 3px;
+			font-size: 13px;
+			background:  #EFF0F2;
+		}
+
+		/* INPUT PLACEHOLDER */
+		input::placeholder{
+			color: #838383;
+			font-size: 13px;
+			font-weight: 100%;
+			font-weight: normal;
+		}
+
+		/* FORM ERROR MESSAGES*/
+		.wrapper .right .text-danger{
+			font-size: 13px;
+			padding-bottom:-1.25em;
+			margin-bottom:-1.25em;
+		}
+
+		/* PASSWORD OR EMAIL ERROR */
+		.wrapper .right .pass{
+			font-size: 16px;
+			margin-top:20px;
+			margin-bottom:none;
+		}
+
+		/* CREATE ACCOUNT BUTTON */
+		.wrapper .right .btn{
+			color: #fff;
+			text-align: center;
+			background:  #8E5758;
+			width: 250px;
+			height: 35px;
+			transform: translate(0%,110%);
+		}
+
+		.wrapper .right .btn:hover{
+			background-color: #260200;
+		}
+	</style>
   </head>
   <body>
 	<div class="wrapper">
